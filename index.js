9de3a855d174fb6cce30e90d25af16b343c6d883
Thyago Ferreira <enfthyagotrm@hotmail.com>
2023-11-29 21:40:08 -0300

    filtro de tarefas ativas completo

--- index.js
+++ index.js
@@ -72,8 +72,26 @@ app.post('/criar', (requisicao, resposta) => {
 
 
 app.get ('/ativas', (requisicao, resposta) =>{
-    
-})
+    const sql = `
+        SELECT * FROM tarefas
+        WHERE completa = 0
+    `
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log (erro)
+        }
+        const tarefas = dados.map((dado) => {
+            return  {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: false
+            }
+        })
+        const quantidadedeTarefas = tarefas.length
+        resposta.render('ativas', {tarefas, quantidadedeTarefas})
+    })
+}) 
     
 
 app.get('/',(requisicao, resposta) =>{
